47c4cf943889b6cf69fd4e798be3e9bbafab413a
Jean Chassoul <chassoul@gmail.com>
2015-06-23 15:32:40 -0600

    coherente?

--- static/js/views/footer.js
+++ static/js/views/footer.js
@@ -1,14 +1,15 @@
 fun.views.footer = Backbone.View.extend({
 
     events: {
-
+        "click #login-btn": "loginStuff",
+        "click #fun-signup": "signupStuff"
     },
 
-    initialize : function(options) {
+    initialize: function(options) {
         fun.containers.footer = this.$el;
     },
     
-    render : function(){
+    render: function(){
         var template = _.template(
             fun.utils.getTemplate(fun.conf.templates.footer)
         );
@@ -20,8 +21,61 @@ fun.views.footer = Backbone.View.extend({
             'backdrop': 'static',
             'keyboard': true
         });
+    },
 
-        console.log('stuff');
+    signupStuff: function(event){
+        event.preventDefault();
+
+        console.log('signup');
+    },
+
+    loginStuff: function(event){
+        event.preventDefault();
+
+        var loginError = this.loginError;
+        var username = this.username.val();
+        var password = this.password.val();
+        var view = this;
+
+        var loginSuccess = function(view, loginError){
+            // Clear the stuff from the inputs ;)
+            view.$('#username').val('');
+            view.$('#password').val('');
+            loginError.removeClass("show" ).addClass("hide");
+            fun.utils.redirect(fun.conf.hash.dashboard);
+        };
+        
+        fun.utils.login(username, password, {
+            success : function(jqXHR, textStatus){
+                // currently this success call is never executed
+                // the success stuff is going on case 200 of the error function.
+                // Why? well... I really don't fucking know...
+                loginSuccess(view, loginError);
+            },
+            error : function(jqXHR, textStatus, errorThrown) {
+                switch(jqXHR.status) {
+                    case 403:
+                        var message = fun.utils.translate("usernameOrPasswordError");
+                        loginError.find('p').html(message);
+                        loginError.removeClass("hide" ).addClass("show");
+                        break;
+                    case 200:
+                        // Check browser support
+                        if (typeof(Storage) != "undefined") {
+                            // Store
+                            localStorage.setItem("username", username);
+                        }
+                        loginSuccess(view, loginError);
+                        $('#loginModal').modal('hide');
+                        break;
+                    default:
+                        console.log('the monkey is down');
+                        break;
+                }
+            }
+        
+        });
+
     }
 
 });
