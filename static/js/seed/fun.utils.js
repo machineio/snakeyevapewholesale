a9528a0ba423565dc507a3e209010cddc497947d
Jean Chassoul <chassoul@gmail.com>
2015-06-23 20:56:03 -0600

    come on!

--- static/js/seed/fun.utils.js
+++ static/js/seed/fun.utils.js
@@ -84,6 +84,7 @@ fun.utils.login = function(account, password, callbacks) {
         url: fun.conf.urls.login,
         dataType: 'json',
         beforeSend: function(xhr){
+            console.log(account, password);
             auth = account + ':' + password;
             var words  = CryptoJS.enc.Latin1.parse(auth);
             var base64 = CryptoJS.enc.Base64.stringify(words);
