728db5cfa068fc443e5e0a916916a8087cc2ac18
Jean Chassoul <chassoul@gmail.com>
2015-10-09 15:24:13 -0600

    no

--- static/js/views/ism.js
+++ static/js/views/ism.js
@@ -1,4 +1,4 @@
-fun.views.tanks = Backbone.View.extend({
+fun.views.ism = Backbone.View.extend({
 
     /*
     * Bind the event functions to the different HTML elements
@@ -11,14 +11,14 @@ fun.views.tanks = Backbone.View.extend({
     * Class constructor
     */
     initialize: function(options){
-        fun.containers.tanks = this.$el;
+        fun.containers.ism = this.$el;
     },
 
     /**
     * Render view
     */
     render: function(){
-        var template = _.template(fun.utils.getTemplate(fun.conf.templates.tanks));
+        var template = _.template(fun.utils.getTemplate(fun.conf.templates.ism));
         this.$el.html(template);
         this.$el.show();
     }
--- static/js/views/waxcdb.js
+++ static/js/views/waxcdb.js
@@ -1,4 +1,4 @@
-fun.views.tanks = Backbone.View.extend({
+fun.views.waxcdb = Backbone.View.extend({
 
     /*
     * Bind the event functions to the different HTML elements
@@ -11,14 +11,14 @@ fun.views.tanks = Backbone.View.extend({
     * Class constructor
     */
     initialize: function(options){
-        fun.containers.tanks = this.$el;
+        fun.containers.waxcdb = this.$el;
     },
 
     /**
     * Render view
     */
     render: function(){
-        var template = _.template(fun.utils.getTemplate(fun.conf.templates.tanks));
+        var template = _.template(fun.utils.getTemplate(fun.conf.templates.waxcdb));
         this.$el.html(template);
         this.$el.show();
     }
