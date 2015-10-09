b8fae45cdec083a87ccd6f2e9181a5a39bbf2238
Jean Chassoul <chassoul@gmail.com>
2015-10-09 09:44:11 -0600

    no

--- static/js/seed/fun.utils.js
+++ static/js/seed/fun.utils.js
@@ -256,6 +256,26 @@ fun.utils.hideAll = function() {
 };
 
 
+fun.utils.hideLanding = function(){
+    $("#landing-00").removeClass("show").addClass("hide");
+    $("#landing-01").removeClass("show").addClass("hide");
+    $("#landing-02").removeClass("show").addClass("hide");
+    $("#landing-03").removeClass("show").addClass("hide");
+    $("#landing-04").removeClass("show").addClass("hide");
+    $("#landing-05").removeClass("show").addClass("hide");
+    $("#call-to-action").removeClass("show").addClass("hide");
+};
+
+fun.utils.showLanding = function(){
+    $("#landing-00").removeClass("hide").addClass("show");
+    $("#landing-01").removeClass("hide").addClass("show");
+    $("#landing-02").removeClass("hide").addClass("show");
+    $("#landing-03").removeClass("hide").addClass("show");
+    $("#landing-04").removeClass("hide").addClass("show");
+    $("#landing-05").removeClass("hide").addClass("show");
+    $("#call-to-action").removeClass("hide").addClass("show");
+};
+
 /**
  * Rounds up a number.
  * @return Object
